46169600ea07b3d05facde1b93c6c6677b1d6147
Anant Doshi <62616624+anant0103@users.noreply.github.com>
2021-02-13 16:17:24 +0530

    First commit

--- Django/DEMOPROJECT/DEMOPROJECT/settings.py
+++ Django/DEMOPROJECT/DEMOPROJECT/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8vq@ec@fnagfz1jv3j4#skxp3akirf_+wg@je9bri-zy_#5yu&'
+SECRET_KEY = 'add secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
